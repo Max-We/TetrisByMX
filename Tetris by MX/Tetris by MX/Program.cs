455d25d6c55b646824258388166b20f1db204735
Max-We <48329936+Max-We@users.noreply.github.com>
2019-04-03 14:20:28 +0200

    Add files via upload

---
+++ Tetris by MX/Tetris by MX/Program.cs
@@ -0,0 +1,189 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.InteropServices;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Tetris_by_MX
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string b = "X";
+
+            int x1, x2, x3, x4, x5, x6, x7, x8;
+            int M = 1;
+
+            Console.SetWindowSize(21, 22);
+            Console.BufferHeight = 22;
+            Console.BufferWidth = 22;
+
+            ConsoleKeyInfo cki;
+            int mx, my;
+
+            // Startwerte
+            mx = Console.BufferWidth / 2;
+            my = 2;
+            // Block 1
+            x1 = x2 = x6 = x7 = x8 = 0;
+            x3 = x4 = x5 = 1;
+
+            // Block 2
+            //x1 = x3 = x4 = x5 = x6 = 0;
+            //x2 = x7 = x8 = 0;
+
+
+            int posX = mx;
+            int posY = my;
+
+            //Start
+            if (x1 != 0)
+            {
+                Console.SetCursorPosition(mx - 1, my + 1);
+                Console.Write(b);
+            }
+            if (x2 != 0)
+            {
+                Console.SetCursorPosition(mx, my + 1);
+                Console.Write(b);
+            }
+            if (x3 != 0)
+            {
+                Console.SetCursorPosition(mx + 1, my + 1);
+                Console.Write(b);
+            }
+            if (x4 != 0)
+            {
+                Console.SetCursorPosition(mx - 1, my);
+                Console.Write(b);
+            }
+            if (x5 != 0)
+            {
+                Console.SetCursorPosition(mx + 1, my);
+                Console.Write(b);
+            }
+            if (x6 != 0)
+            {
+                Console.SetCursorPosition(mx - 1, my - 1);
+                Console.Write(b);
+            }
+            if (x7 != 0)
+            {
+                Console.SetCursorPosition(mx, my - 1);
+                Console.Write(b);
+            }
+            if (x8 != 0)
+            {
+                Console.SetCursorPosition(mx + 1, my - 1);
+                Console.Write(b);
+            }
+
+            // Drehen
+            int counter = 0;
+            cki = Console.ReadKey();
+            while (Console.ReadKey() != new ConsoleKeyInfo())
+            {
+
+
+                switch (cki.Key)
+                {
+                    case ConsoleKey.RightArrow:
+                        if (counter == 0)
+                        {
+                            x1 = x3 = x4 = x5 = x6 = 0;
+                            x2 = x7 = x8 = 0;
+                            counter = 1;
+                        }
+                        else if (counter == 1)
+                        {
+                            x1 = x2 = x3 = x7 = x8 = 0;
+                            x4 = x5 = x6 = 1;
+                            counter = 2;
+
+                        }
+                        else if (counter == 2)
+                        {
+                            x3 = x4 = x5 = x6 = x8 = 0;
+                            x1 = x2 = x7 = 1;
+                            counter = 3;
+                        }
+                        else if (counter == 3)
+                        {
+                            x1 = x2 = x6 = x7 = x8 = 0;
+                            x3 = x4 = x5 = 1;
+                            counter = 0;
+                        }
+                        break;
+
+                }
+
+                Console.SetCursorPosition(mx - 1, my + 1);
+                Console.Write("");
+                Console.SetCursorPosition(mx, my + 1);
+                Console.Write("");
+                Console.SetCursorPosition(mx + 1, my + 1);
+                Console.Write("");
+                Console.SetCursorPosition(mx - 1, my);
+                Console.Write("");
+                Console.SetCursorPosition(mx + 1, my);
+                Console.Write("");
+                Console.SetCursorPosition(mx - 1, my - 1);
+                Console.Write("");
+                Console.SetCursorPosition(mx, my - 1);
+                Console.Write("");
+                Console.SetCursorPosition(mx + 1, my - 1);
+                Console.Write("");
+
+                Console.SetCursorPosition(mx, my);
+                Console.Write(b);
+
+                if (x1 != 0)
+                {
+                    Console.SetCursorPosition(mx - 1, my + 1);
+                    Console.Write(b);
+                }
+                if (x2 != 0)
+                {
+                    Console.SetCursorPosition(mx, my + 1);
+                    Console.Write(b);
+                }
+                if (x3 != 0)
+                {
+                    Console.SetCursorPosition(mx + 1, my + 1);
+                    Console.Write(b);
+                }
+                if (x4 != 0)
+                {
+                    Console.SetCursorPosition(mx - 1, my);
+                    Console.Write(b);
+                }
+                if (x5 != 0)
+                {
+                    Console.SetCursorPosition(mx + 1, my);
+                    Console.Write(b);
+                }
+                if (x6 != 0)
+                {
+                    Console.SetCursorPosition(mx - 1, my - 1);
+                    Console.Write(b);
+                }
+                if (x7 != 0)
+                {
+                    Console.SetCursorPosition(mx, my - 1);
+                    Console.Write(b);
+                }
+                if (x8 != 0)
+                {
+                    Console.SetCursorPosition(mx + 1, my - 1);
+                    Console.Write(b);
+                }
+
+            }
+
+            Console.ReadKey();
+
+        }
+    }
+}
---
+++ Tetris by MX/Tetris by MX/Properties/AssemblyInfo.cs
@@ -0,0 +1,36 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// Allgemeine Informationen über eine Assembly werden über die folgenden
+// Attribute gesteuert. Ändern Sie diese Attributwerte, um die Informationen zu ändern,
+// die einer Assembly zugeordnet sind.
+[assembly: AssemblyTitle("Tetris by MX")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("Tetris by MX")]
+[assembly: AssemblyCopyright("Copyright ©  2019")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// Durch Festlegen von ComVisible auf FALSE werden die Typen in dieser Assembly
+// für COM-Komponenten unsichtbar.  Wenn Sie auf einen Typ in dieser Assembly von
+// COM aus zugreifen müssen, sollten Sie das ComVisible-Attribut für diesen Typ auf "True" festlegen.
+[assembly: ComVisible(false)]
+
+// Die folgende GUID bestimmt die ID der Typbibliothek, wenn dieses Projekt für COM verfügbar gemacht wird
+[assembly: Guid("b162c9d3-f5f2-449a-9241-67e4e06ba2f5")]
+
+// Versionsinformationen für eine Assembly bestehen aus den folgenden vier Werten:
+//
+//      Hauptversion
+//      Nebenversion
+//      Buildnummer
+//      Revision
+//
+// Sie können alle Werte angeben oder Standardwerte für die Build- und Revisionsnummern verwenden,
+// übernehmen, indem Sie "*" eingeben:
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
